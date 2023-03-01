f76034d9f41b91346b6ebe09ddfdee5189e190eb
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-03-01 12:52:32 +0330

    work on admin section and write test

--- todo/todos/tests.py
+++ todo/todos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.todo = Todo.objects.create(
+            title = 'Test Title',
+            body = 'Test Body',
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, 'Test Title')
+        self.assertEqual(self.todo.body, 'Test Body')
+        self.assertEqual(str(self.todo), 'Test Title')
