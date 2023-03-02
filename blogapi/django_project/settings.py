40e4903d2a63f6a7d5237c43863951377f2050f4
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-03-02 14:33:32 +0330

    write apis

--- blogapi/django_project/settings.py
+++ blogapi/django_project/settings.py
@@ -37,10 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party
+    'rest_framework',
     # Local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
