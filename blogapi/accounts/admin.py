91c0bf621552f3d7e793794cde71a297c7e9a30d
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-03-02 11:38:13 +0330

    work on custom user model

--- blogapi/accounts/admin.py
+++ blogapi/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        'username',
+        'email',
+        'name',
+        'is_staff',
+    ]
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('name',)}),)
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('name',)}),)
