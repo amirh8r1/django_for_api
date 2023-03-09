24580fde54cd71c1a20f9e07bfc568d51af01a68
Amir Hossein Rajabi <108143052+amirh8r1@users.noreply.github.com>
2023-03-09 17:26:30 +0330

    Viewsets and Routers

--- blogapi/posts/serializers.py
+++ blogapi/posts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
@@ -11,3 +12,11 @@ class PostSerializer(serializers.ModelSerializer):
             'created_at',
         ]
         model = Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = [
+            'id',
+            'username',
+        ]
+        model = get_user_model()
--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet # PostList, PostDetail, UserList, UserDetail
 
-urlpatterns = [
-    path('', PostList.as_view(), name='post_list'),
-    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
-]
+# urlpatterns = [
+#     path('', PostList.as_view(), name='post_list'),
+#     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+#     path('users/', UserList.as_view(), name='user_list'),
+#     path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
+# ]
+
+router = SimpleRouter()
+router.register("users", UserViewSet, basename='users')
+router.register("", PostViewSet, basename='posts')
+
+urlpatterns = router.urls
